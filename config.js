2844e724c4eda91f206c5d1db28447716770ecec
Kaguya Shinomiya <102699105+RyhnXD@users.noreply.github.com>
2022-08-13 17:02:22 +0000

    Update config.js

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 
 // Wajib diisi ya kak!, kalo ga punya biarin aja, biar ada creditsnya :v
 global.sig = 'https://www.instagram.com/kontol_elite'
-global.sgh = 'https://github.com/Jikarinka'
+global.sgh = 'https://github.com/chisato12'
 global.sgc = 'https://chat.whatsapp.com/FF1Bd1U0FHu42rfz6uplbn'
 global.sdc = " "
 global.snh = 'https://nhentai.net/g/365296/'
