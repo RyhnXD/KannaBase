7723acc5764e136f58ec6800dcba5508f42962a6
Kaguya Shinomiya <102699105+RyhnXD@users.noreply.github.com>
2022-08-14 01:13:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -128,7 +128,7 @@ global.author = global.wm
 global.thumb = 'https://telegra.ph/file/04012986020f8fed15131.jpg' //Main Thumbnail
 global.thumb2 = 'https://telegra.ph/file/04012986020f8fed15131.jpg'
 global.thumbbc = 'https://telegra.ph/file/04012986020f8fed15131.jpg' //For broadcast
-global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
+global.giflogo = 'https://telegra.ph/file/d0d1c03fece63364efc2f.jpg'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
